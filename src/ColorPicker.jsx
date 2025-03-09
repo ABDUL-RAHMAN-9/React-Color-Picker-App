3fbf42b2679bffa0a4a3e8e8bb1c6b8ba995af99
Abdul Rahman <abdulrahman161004@gmail.com>
2025-03-09 20:08:21 +0530

    Fixed the outer Div to be center

--- src/ColorPicker.jsx
+++ src/ColorPicker.jsx
@@ -11,14 +11,19 @@ function ColorPicker()
     }
     return (
         <>
-            <div className="flexflex-col items-center ">
+            <div className="flex flex-col items-center justify-center min-h-screen">
                 <div className='flex flex-col items-center justify-center w-100 border m-5 rounded-2xl p-5 space-y-5'>
                     <h1 className='text-4xl font-bold font-serif'>Color Picker</h1>
                     <div className="w-70 h-70 rounded-2xl flex justify-center items-center border-cyan-200 border-4 transition-all 0.25 ease-in-out" style={{ backgroundColor: color }}>
                         <p>Selected Color: {color}</p>
                     </div>
                     <label htmlFor="" className='text-2xl'>Set Color:</label>
-                    <input type="color" className='w-20 h-12 p-1.5 rounded-lg border-2 border-solid' value={color} onChange={handleColorChange} />
+                    <input
+                        type="color"
+                        className='w-20 h-12 p-1.5 rounded-lg border-2 border-solid'
+                        value={color}
+                        onChange={handleColorChange}
+                    />
                 </div>
             </div>
         </>
